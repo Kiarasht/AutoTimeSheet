902005a9bb5638dbc59242e03655a8ae5f6f8988
Kiarasht <kiarashbikar@gmail.com>
2016-11-01 21:57:15 -0700

    Changes:
    - Notification for enter and exit
    - Layout changes, multiple activities now
    - Run time permissions
    - Need to have a dialogfragment before going to GeofenceActivity

--- app/src/main/java/com/restart/autotimesheet/GeofenceService.java
+++ app/src/main/java/com/restart/autotimesheet/GeofenceService.java
@@ -1,18 +1,45 @@
 package com.restart.autotimesheet;
 
 import android.app.IntentService;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.util.Log;
 
 import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.GeofencingEvent;
 
+import java.util.List;
+
 public class GeofenceService extends IntentService {
 
     private static final String TAG = ".GeofencingService";
+    private static final int NOTIFICATION_ID = 1234;
+    private Context mContext;
 
     public GeofenceService() {
         super(TAG);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mContext = getBaseContext();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+
     }
 
     @Override
@@ -25,16 +52,57 @@ public class GeofenceService extends IntentService {
             int transition = event.getGeofenceTransition();
 
             // If you have more than one geofence this is one way to keep track. Using requestId (MyGeofenceId)
-/*            List<Geofence> geofences = event.getTriggeringGeofences();
-            Geofence geofence = geofences.get(0);
-            String requestId = geofence.getRequestId(); */
+            List<Geofence> geofences = event.getTriggeringGeofences();
 
-            // TODO How about we have a notification here instead of a Log?
-            if (transition == Geofence.GEOFENCE_TRANSITION_ENTER) {
-                Log.d(TAG, "Entering geofence area");
-            } else if (transition == Geofence.GEOFENCE_TRANSITION_EXIT) {
-                Log.d(TAG, "Exiting geofence area");
+            for (Geofence aGeofence : geofences) {
+                String requestId = aGeofence.getRequestId();
+
+                if (transition == Geofence.GEOFENCE_TRANSITION_ENTER) {
+                    notification("You have entered area " + requestId + " !");
+                } else if (transition == Geofence.GEOFENCE_TRANSITION_EXIT) {
+                    notification("You have exited area " + requestId + " !");
+                    onDestroy();
+                }
             }
         }
+    }
+
+    /**
+     * Notification system that is used for this app. All we need to do is call this function
+     * when we need to trigger a notification.
+     */
+    private void notification(String message) {
+        Uri soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        Bitmap icon = BitmapFactory.decodeResource(mContext.getResources(), R.mipmap.ic_launcher);
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setAutoCancel(true)
+                        .setOnlyAlertOnce(true)
+                        .setOngoing(false)
+                        .setContentTitle("Auto Time Sheet")
+                        .setContentText(message)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setLargeIcon(icon)
+                        .setSound(soundUri)
+                        .setWhen(System.currentTimeMillis());
+
+        Intent resultIntent = new Intent(mContext, MainActivity.class);
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+
+        stackBuilder.addParentStack(MainActivity.class);
+
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
+
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
     }
 }
--- app/src/main/java/com/restart/autotimesheet/MainActivity.java
+++ app/src/main/java/com/restart/autotimesheet/MainActivity.java
@@ -2,16 +2,19 @@ package com.restart.autotimesheet;
 
 import android.Manifest;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
-import android.location.Location;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
@@ -20,8 +23,6 @@ import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.GeofencingRequest;
-import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
 import java.util.ArrayList;
@@ -29,16 +30,14 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = ".MainActivity";
-    private static final String GEOFENCE_ID = "MyGeofenceId";
+    private static final int REQUEST_PERMISSION_LOCATION = 1234;
 
     private GoogleApiClient mGoogleApiClient;
+    private Context mContext;
 
     /**
      * Set up the basic button layouts, set up the OnClickListeners, and GoogleApiClient with the
      * location permissions it requires to function.
-     * <p>
-     * OnCreate is the root of an activity life cycle.
-     * http://www.javatpoint.com/images/androidimages/Android-Activity-Lifecycle.png
      *
      * @param savedInstanceState N/A
      */
@@ -46,34 +45,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG, "onCreate");
+        mContext = getApplicationContext();
 
-        // Initialize all the three buttons
-        Button startLocationMonitoring = (Button) findViewById(R.id.startLocationMonitoring);
-        Button startGeofenceMonitoring = (Button) findViewById(R.id.startGeofenceMonitoring);
-        Button stopGeofenceMonitoring = (Button) findViewById(R.id.stopGeofenceMonitoring);
-
-        // OnClickListener for starting a location listener
-        startLocationMonitoring.setOnClickListener(new View.OnClickListener() {
+        Button button = (Button) findViewById(R.id.startLocationMonitoring);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startLocationMonitoring();
-            }
-        });
-
-        // OnClickListener for starting the geofence functionality
-        startGeofenceMonitoring.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startGeofenceMonitoring();
-            }
-        });
-
-        // OnClickListener for stopping the geofence functionality
-        stopGeofenceMonitoring.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                stopGeofenceMonitoring();
+                startActivity(new Intent(mContext, GeofenceActivity.class));
             }
         });
 
@@ -96,11 +74,6 @@ public class MainActivity extends AppCompatActivity {
                         Log.e(TAG, "GoogleApiClient failed to connect! Boo!");
                     }
                 }).build();
-
-        // Get location permissions if on Marshmallow 6.0 or above
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1234);
-        }
     }
 
     /**
@@ -109,12 +82,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d(TAG, "onResume");
 
-        /**
-         * Require the user to install Google services otherwise the app won't work.
-         * http://www.abcdiamond.com/images/Get-Google-Play-services-300x153.jpg
-         */
+        // Require the user to install Google services otherwise the app won't work.
         int response = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this);
         if (response != ConnectionResult.SUCCESS) {
             GoogleApiAvailability.getInstance().getErrorDialog(this, response, 1).show();
@@ -127,8 +96,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d(TAG, "onStart");
-
         mGoogleApiClient.reconnect();
     }
 
@@ -138,33 +105,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d(TAG, "onStop");
-
         mGoogleApiClient.disconnect();
-    }
-
-    /**
-     * Start asking for location updates using the GoogleApiClient.
-     */
-    private void startLocationMonitoring() {
-        try {
-            // Setup a location request
-            LocationRequest locationRequest = LocationRequest.create()
-                    .setInterval(10000)
-                    .setFastestInterval(5000)
-                    .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-
-            // Start listening for location updates using the GoogleApiClient, previous location request criteria, and a listener
-            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, locationRequest, new LocationListener() {
-                @Override
-                public void onLocationChanged(Location location) {
-                    Log.d(TAG, "Location: " + location.getLatitude() + " " + location.getLongitude());
-                }
-            });
-        } catch (SecurityException e) { // Throw a SecurityException if permissions were not allowed
-            Log.wtf(TAG, "Shouldn't happen, we have the location permissions. Wait you hit deny didn't you?");
-            e.printStackTrace();
-        }
     }
 
     /**
@@ -174,7 +115,7 @@ public class MainActivity extends AppCompatActivity {
         try {
             // Set up a geofence criteria by giving it an id, location + radius, responsiveness, transitions, etc...
             Geofence geofence = new Geofence.Builder()
-                    .setRequestId(GEOFENCE_ID)
+                    .setRequestId("Job")
                     .setCircularRegion(45.545184, -122.845018, 100)
                     .setExpirationDuration(Geofence.NEVER_EXPIRE)
                     .setNotificationResponsiveness(1000)
@@ -193,7 +134,7 @@ public class MainActivity extends AppCompatActivity {
 
             // Now start geofence when all is well
             if (!mGoogleApiClient.isConnected()) {
-                Log.e(TAG, "GoogleApiClient isn't connect!");
+                throw new SecurityException("Unable to connect to servers");
             } else {
                 LocationServices.GeofencingApi.addGeofences(mGoogleApiClient, geofencingRequest, pendingIntent)
                         .setResultCallback(new ResultCallback<Status>() {
@@ -207,8 +148,8 @@ public class MainActivity extends AppCompatActivity {
                             }
                         });
             }
-        } catch (SecurityException e) { // It can fail if user denies the location permission on >= 6.0
-            Log.wtf(TAG, "Shouldn't happen, we have the location permissions. Wait you hit deny didn't you?");
+        } catch (SecurityException e) {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
             e.printStackTrace();
         }
     }
@@ -218,7 +159,71 @@ public class MainActivity extends AppCompatActivity {
      */
     private void stopGeofenceMonitoring() {
         ArrayList<String> geofenceIds = new ArrayList<>();
-        geofenceIds.add(GEOFENCE_ID);
+        //geofenceIds.add(GEOFENCE_Job);
         LocationServices.GeofencingApi.removeGeofences(mGoogleApiClient, geofenceIds);
+    }
+
+    public void newGeofence(View view) {
+        String[] locationPermission = new String[]{Manifest.permission.ACCESS_FINE_LOCATION};
+
+        if (isPermissionGranted(locationPermission)) {
+            startActivity(new Intent(mContext, GeofenceActivity.class));
+        } else {
+            getPermission(locationPermission);
+        }
+    }
+
+    /**
+     * Check to see if user has given us the permission(s) to access something. Location, camera, etc...
+     *
+     * @param permissions A string array of permission(s) that is being checked
+     * @return Returns true if permission(s) are granted, and false if denied
+     */
+    private boolean isPermissionGranted(String[] permissions) {
+        if (Build.VERSION.SDK_INT >= 23) {
+            boolean allGiven = true;
+            for (String aPermission : permissions) {
+                if (mContext.checkSelfPermission(aPermission) != PackageManager.PERMISSION_GRANTED) {
+                    allGiven = false;
+                    break;
+                }
+            }
+            return allGiven;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * Gets called when we need to get permissions from the user
+     *
+     * @param permission The permission(s) we need from user.
+     */
+    private void getPermission(String[] permission) {
+        ActivityCompat.requestPermissions(MainActivity.this, permission, REQUEST_PERMISSION_LOCATION);
+    }
+
+    /**
+     * Switch case for each of the permission cases for managing each of the requests and their
+     * results separately.
+     *
+     * @param requestCode  Used for calling the correct switch case
+     * @param permissions  Array of permissions that were requested
+     * @param grantResults Result from the permissions
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_PERMISSION_LOCATION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                        && grantResults[1] == PackageManager.PERMISSION_GRANTED
+                        && grantResults[2] == PackageManager.PERMISSION_GRANTED) {
+                    startActivity(new Intent(mContext, GeofenceActivity.class));
+                } else {
+                    Toast.makeText(mContext, "Need location permission to track a time sheet", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
     }
 }
